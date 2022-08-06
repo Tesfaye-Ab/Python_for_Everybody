b7fb0c1b192d8ea4e5fd0e82a20c3c059cc0335b
Tesfaye-Ab <106488308+Tesfaye-Ab@users.noreply.github.com>
2022-08-06 23:38:25 +0200

    version 2

--- ex_03.py
+++ ex_03.py
@@ -1,8 +1,8 @@
 #Write a program to prompt the user for hours and rate per hour to compute
 #gross pay.
-hours = int(input("Enter Hours: "))
+hours = float(input("Enter Hours: "))
 rate = float(input("Enter Rate: "))
 
 pay = hours * rate
- 
+
 print("pay: ", pay)
